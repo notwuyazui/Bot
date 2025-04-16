be80d80c0e228320ccb4cfe0aa6b63408219dba7
notwuyazui <3314717601@qq.com>
2025-04-16 18:30:10 +0800

    add a env file

---
+++ ChatBot/demo.bat
@@ -0,0 +1,11 @@
+python -m pip install --user pipx
+python -m pipx ensurepath
+pipx install nb-cli
+cd JMBot/
+python -m venv .venv --prompt nonebot2
+.venv\Scripts\activate
+pip install 'nonebot2[fastapi]'
+pip install nonebot-adapter-console
+pip install nonebot-plugin-jmcomic
+nb plugin install nonebot-plugin-deepseek
+python bot.py
